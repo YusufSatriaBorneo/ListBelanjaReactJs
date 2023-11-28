4680926c71b15911869df78cce412e40201872d9
Yusuf Satria Borneo <yusufsatriaborneo@gmail.com>
2023-11-28 17:42:19 +0800

    Update final

---
+++ src/componen/GroceryList.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import Item from "./Item";
+
+const GroceryList = ({ items, onDeleteItem, onToggleItem, onClearItems }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  // supaya jadi satu di seperti yang bawah
+  // if (sortBy === "input") {
+  //   sortedItems = items;
+  // }
+  // // javascrip untuk mengurutkan cek di internet sorted
+  // if (sortBy === "name") {
+  //   sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+  // }
+
+  // if (sortBy === "checked") {
+  //   sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
+  // }
+
+  switch (sortBy) {
+    case "name":
+      sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+      break;
+
+    case "checked":
+      sortedItems = items.slice().sort((a, b) => a.checked - b.checked);
+      break;
+
+    default:
+      sortedItems = items;
+      break;
+  }
+
+  return (
+    <>
+      <div className="list">
+        <ul>
+          {sortedItems.map((item) => (
+            <Item
+              item={item}
+              key={item.id}
+              onDeleteItem={onDeleteItem}
+              onToggleItem={onToggleItem}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Urutkan berdasarkan urutan input</option>
+          <option value="name">Urutkan berdasarkan nama barang</option>
+          <option value="checked">Urutkan berdasarkan ceklis</option>
+        </select>
+        <button onClick={onClearItems}>Bersihkan Daftar</button>
+      </div>
+    </>
+  );
+};
+export default GroceryList;
